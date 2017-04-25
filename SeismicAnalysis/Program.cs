18ec404c377ad40571d596d1adb8d0896989147d
Jack Steel <jacksteel@jacksteel.co.uk>
2017-04-25 18:47:10 +0100

    adds output

--- SeismicAnalysis/Program.cs
+++ SeismicAnalysis/Program.cs
@@ -1,60 +1,162 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace SeismicAnalysis {
-    class Program {
+
+    internal class Program {
+
         //year
         private static int[] year;
+
         //month
         private static string[] month;
+
         //day
         private static int[] day;
+
         //time
         private static string[] time;
+
         //magnitude
         private static decimal[] mag;
+
         //latitude
         private static decimal[] lat;
+
         //longitude
         private static decimal[] lon;
+
         //depth
         private static decimal[] depth;
+
         //region
         private static string[] region;
+
         //IRIS_ID
         private static string[] id;
-        //timestamp 
+
+        //timestamp
         private static long[] timestamp;
 
-        static void Main(string[] args) {
+        //import for maximising the console window from: http://stackoverflow.com/questions/22053112/maximizing-console-window-c-sharp
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(System.IntPtr hWnd, int cmdShow);
+
+        private static void Main(string[] args) {
+
+            //maximise the window
+            Process p = Process.GetCurrentProcess();
+            ShowWindow(p.MainWindowHandle, 3);
+            Console.SetBufferSize(9001, 9001);
+            
             //read in files: *_1.txt
             populateArraysOne();
             SeismicRecord[] data1 = new SeismicRecord[year.Length];
             data1 = fillRecordArray();
-            selectDataToAnalyse();
+            outputCurrentState(data1);
+/*
+            
+            string selected = selectDataToAnalyse();
+            data1 = sortSelectedData(selected, data1);
+            outputCurrentState(data1);
+            */
+            Console.Read();
         }
 
         //task 1
-        private static int selectDataToAnalyse() {
+        private static string selectDataToAnalyse() {
             //output options
             Console.WriteLine("Select what data you want to analyse: ");
             Console.WriteLine("1:\tYear\n2:\tMonth\n3:\tDay\n4:\tTime\n5:\tMagnitude\n6:\tLatitude\n7:\tLongitude\n8:\tDepth\n9:\tRegion\n10:\tIRIS ID\n11:\tTimestamp\n");
             int selection = getUserInput(1, 11);
-            return selection;
+            switch (selection) {
+                case 1:
+                    return "Year";
+
+                case 2:
+                    return "Month";
+
+                case 3:
+                    return "Day";
+
+                case 4:
+                    return "Time";
+
+                case 5:
+                    return "Magnitude";
+
+                case 6:
+                    return "Latitude";
+
+                case 7:
+                    return "Longitude";
+
+                case 8:
+                    return "Depth";
+
+                case 9:
+                    return "Region";
+
+                case 10:
+                    return "ID";
+
+                case 11:
+                    return "Timestamp";
+
+                default:
+                    return "Year";
+            }
+        }
+
+        //task 2
+        private static SeismicRecord[] sortSelectedData(string dataSelected, SeismicRecord[] data) {
+            Console.WriteLine("How do you want to sort this data?");
+            Console.WriteLine("1:\tAscending order\n2:\tDescending order\n");
+            int selection = getUserInput(1, 2);
+            switch (selection) {
+                case 2:
+                    SeismicRecord[] temp = sortDataAscending(dataSelected, data);
+                    //reverse the ascending order array to get a decending order one
+                    SeismicRecord[] output = new SeismicRecord[temp.Length];
+                    int count = 0;
+                    for (int i = temp.Length - 1; i <= 0; i--) {
+                        output[count] = temp[i];
+                    }
+                    return output;
+                default:
+                    return sortDataAscending(dataSelected, data);
+            }
         }
 
 
-        private static int getUserInput(int lowerBound, int upperBound) {            
+        private static SeismicRecord[] sortDataAscending(string selector, SeismicRecord[] data) {
+            PropertyInfo p = data[0].GetType().GetProperty(selector);
+            int steps = 0;
+            Sorting.quickSort(ref data, 0, data.Length - 1, p, ref steps);
+            Console.WriteLine("Sorted in {0} steps using quicksort", steps);
+            return data;
+        }
+
+        //task 7 and 5
+        private static void outputCurrentState(SeismicRecord[] data) {
+            //headers
+            Console.WriteLine("{0,25}\t|\t{1,25}\t|\t{2,25}\t|\t{3,25}\t|\t{4,25}\t|\t{5,25}\t|\t{6,25}\t|\t{7,25}\t|\t{8,25}\t|\t{9,25}\t|\t{10,25}","Year", "Month", "Day", "Time","Magnitude","Latitude","Longitude","Depth","Region", "IRIS ID", "Timestamp" );
+            //data
+            foreach (SeismicRecord s in data) {
+                Console.WriteLine("{0,25}\t|\t{1,25}\t|\t{2,25}\t|\t{3,25}\t|\t{4,25}\t|\t{5,25}\t|\t{6,25}\t|\t{7,25}\t|\t{8,25}\t|\t{9,25}\t|\t{10,25}", s.Year, s.getMonth(), s.getDay(), s.Time, s.Magnitude, s.Lat, s.Lon, s.Depth, s.Region, s.ID, s.Timestamp);
+            }
+        }
+
+        private static int getUserInput(int lowerBound, int upperBound) {
             bool valid = false;
             int val = 0;
             do {
-                string input = Console.ReadLine();              
-                if (int.TryParse(input,out val)) {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out val)) {
                     if (val >= lowerBound && val <= upperBound) {
-                        valid = true;                       
+                        valid = true;
                     }
                 }
                 if (!valid) {
@@ -70,7 +172,7 @@ namespace SeismicAnalysis {
         /// <returns>An array of SeismicRecord objects</returns>
         private static SeismicRecord[] fillRecordArray() {
             SeismicRecord[] data = new SeismicRecord[year.Length];
-            for(int i = 0; i < year.Length; i++) {
+            for (int i = 0; i < year.Length; i++) {
                 data[i] = new SeismicRecord(year[i], month[i], day[i], time[i], mag[i], lat[i], lon[i], depth[i], region[i], id[i], timestamp[i]);
             }
             return data;
--- SeismicAnalysis/SeismicRecord.cs
+++ SeismicAnalysis/SeismicRecord.cs
@@ -5,12 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SeismicAnalysis {
-    class SeismicRecord {
+    class SeismicRecord : ICloneable{
         private int year;
-        private string month;
+        private int month;
         private int day;
         private string time;
-        private decimal mag;
+        public decimal mag;
         private decimal lat;
         private decimal lon;
         private decimal depth;
@@ -24,7 +24,7 @@ namespace SeismicAnalysis {
             }
         }
 
-        public string Month {
+        public int Month {
             get {
                 return this.month;
             }
@@ -85,8 +85,45 @@ namespace SeismicAnalysis {
         }
 
         public SeismicRecord(int year, string month, int day, string time, decimal magnitude, decimal lat, decimal lon, decimal depth, string region, string id, long timestamp) {
-            this.year = year;
-            this.month = month;
+            this.year = year;            
+            switch (month.Trim().ToLower()) {
+                case "january":
+                    this.month = 1;
+                    break;
+                case "february":
+                    this.month = 2;
+                    break;
+                case "march":
+                    this.month = 3;
+                    break;
+                case "april":
+                    this.month = 4;
+                    break;
+                case "may":
+                    this.month = 5;
+                    break;
+                case "june":
+                    this.month = 6;
+                    break;
+                case "july":
+                    this.month = 7;
+                    break;
+                case "august":
+                    this.month = 8;
+                    break;
+                case "september":
+                    this.month = 9;
+                    break;
+                case "october":
+                    this.month = 10;
+                    break;
+                case "november":
+                    this.month = 11;
+                    break;
+                case "december":
+                    this.month = 12;
+                    break;
+            }
             this.day = day;
             this.time = time;
             this.mag = magnitude;
@@ -96,7 +133,20 @@ namespace SeismicAnalysis {
             this.region = region;
             this.id = id;
             this.timestamp = timestamp;
+        }
 
+        public string getMonth() {
+            DateTime d = new DateTime(year, month, day);
+            return d.ToString("MMMM");
+        }
+
+        public string getDay() {
+            DateTime d = new DateTime(year, month, day);
+            return d.ToString("dd");
+        }
+
+        public object Clone() {
+            return this.MemberwiseClone();
         }
     }
 }
